267a1eb6e09438dc6afc7f491f9676cd212cdce3
vamsidhar muthireddy <vamsi.muthireddy@gmail.com>
2022-08-04 09:35:08 +0530

    Basic Working copy of cli

---
+++ pureml/cli/main.py
@@ -0,0 +1,77 @@
+import typer
+from pureml.config.parser import Config
+from pureml.trainer.train import Trainer
+import os
+
+app = typer.Typer()
+
+@app.command('generate')
+def generate(project : str = typer.Option(..., '--project'), 
+                data : str = typer.Option(..., '--data'), 
+                output_parameters: str = typer.Option(..., '--output_parameters')):
+    '''
+    Generates the config file from input parameters
+    '''
+    # pass
+
+    os.makedirs(project, exist_ok=True)
+    
+    config_path = os.path.join(project, 'config.yaml')
+    
+    config = Config(config_path=config_path)
+    
+    config.create_config(project=project, label=output_parameters, data=data)
+    config.load_config()
+    config.generate_config()
+
+
+    
+
+
+@app.command('train')
+def train(project : str = typer.Option(..., '--project')):
+    '''
+    Trains the model from the config file in project folder
+    '''
+    
+    config_path = os.path.join(project, 'config.yaml')
+    config = Config(config_path=config_path)
+    config.load_config()
+    
+    trainer = Trainer(label_header=config.label_header, data_path=config.data_path, project_path=config.project_folder)
+
+    trainer.load_data()
+
+    trainer.fit()
+
+    # trainer.app.launch(share=True)
+
+
+
+
+@app.command('predict')
+def predict():
+    '''
+    Predicts the output of the model in the given project directory for the given data
+    '''
+    pass
+
+
+@app.command('launch_gradio')
+def launch_gradio(project : str = typer.Option(..., '--project')):
+    config_path = os.path.join(project, 'config.yaml')
+    config = Config(config_path=config_path)
+    config.load_config()
+    
+    trainer = Trainer(label_header=config.label_header, data_path=config.data_path, project_path=config.project_folder)
+
+    trainer.load_data()
+    trainer.load_model()
+    
+    trainer.create_gradio()
+
+    trainer.app.launch()
+
+
+if __name__ == "__main__":
+    app()
---
+++ pureml/config/parser.py
@@ -0,0 +1,97 @@
+from distutils.command.config import config
+import yaml
+from pydantic import BaseModel
+import typing
+import pandas as pd
+import pyarrow.parquet as pq
+import os
+from collections import OrderedDict, defaultdict
+
+class Config(BaseModel):
+    config_path: str = None
+    config: dict = None
+
+    input_parameters: typing.Any = None
+    output_parameters: typing.Any = None
+    label_header : typing.Any = None
+
+    data_path: typing.Any = None
+    project_folder: typing.Any = None
+
+
+    def create_config(self, project, label, data):
+        config_dict = OrderedDict()
+        # config_dict = defaultdict()
+
+        config_dict['Version'] = 0.1
+
+        config_dict['Organization'] = {
+                    'project' : {
+                        'name' : project, 
+                        'version' : 1
+                    },
+                    'data' : {
+                        'name' : 'Project Data',
+                        'type' : 'url',
+                        'format' : 'parquet',
+                        'path' : data
+                    }
+        }
+
+
+        parquet_file = pq.read_table(data, columns=['A']).to_pandas()
+        
+        config_dict['Output paramters'] = {
+                        'name' : label,
+                        'type' : str(parquet_file.dtypes[label])
+        }
+
+
+        config_dict = dict(config_dict)
+
+
+        with open(os.path.join(project, 'config.yaml'), "w") as f:
+            yaml.dump(config_dict, f, sort_keys=False)
+
+
+
+    def load_config(self):
+        self.config = yaml.load(open(self.config_path, 'r'), Loader=yaml.SafeLoader)
+
+        self.data_path = self.config['Organization']['data']['path']
+        self.project_folder = self.config['Organization']['project']['name']
+        os.makedirs(self.project_folder, exist_ok=True)
+
+
+        self.label_header = self.config['Output paramters']['name']
+
+
+    def generate_config(self):
+
+        parquet_file = pq.read_table(self.data_path)
+        file_metadata = parquet_file.schema.pandas_metadata
+
+
+        columns_input = []
+
+        for column in file_metadata['columns']:
+            if column['name'] == column['field_name']:
+                columns_input.append({'name': column['name'], 'type':column['numpy_type']})
+
+        
+        self.config['Input paramters'] = columns_input
+
+        # self.config
+
+        with open(os.path.join(self.project_folder, 'config.yaml'), "w") as f:
+                # json.dump(project_params, f)
+            # print(project_params)
+            yaml.dump(self.config, f, sort_keys=False)
+
+
+
+
+        
+
+
+
