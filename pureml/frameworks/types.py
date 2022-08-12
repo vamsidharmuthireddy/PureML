4e2917aebf1645054254d40c2069a3f7d8d72d6d
vamsidhar muthireddy <vamsi.muthireddy@gmail.com>
2022-08-12 06:15:06 +0530

    Added mlflow for saving models

---
+++ pureml/frameworks/types.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any
+
+
+class ModelFrameworkType(Enum):
+    SKLEARN = "sklearn"
+    TENSORFLOW = "tensorflow"
+    KERAS = "keras"
+    PYTORCH = "pytorch"
+    HUGGINGFACE_TRANSFORMER = "huggingface_transformer"
+    XGBOOST = "xgboost"
+    LIGHTGBM = "lightgbm"
+    CUSTOM = "custom"
+
+
+@dataclass
+class Example:
+    name: str
+    input: Any
+
+    @staticmethod
+    def from_dict(example_dict):
+        return Example(
+            name=example_dict["name"],
+            input=example_dict["input"],
+        )
+
+    def to_dict(self) -> dict:
+        return {
+            "name": self.name,
+            "input": self.input,
+        }
