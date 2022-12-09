6b2c5ce4833022fbab10180218f3db6ffa95bbd5
vamsidhar muthireddy <vamsi.muthireddy@gmail.com>
2022-12-09 17:56:54 +0530

    Fixed pipeline creation issue if parent node is not available in node list.

--- pureml/utils/constants.py
+++ pureml/utils/constants.py
@@ -6,12 +6,12 @@ PATH_USER_PROJECT_DIR = os.path.join(os.getcwd(),'.pureml')
 PATH_CONFIG = os.path.join(PATH_USER_PROJECT_DIR,'config.pkl')# 'temp.yaml'
 
 PATH_ARTIFACT_DIR = os.path.join(PATH_USER_PROJECT_DIR,'artifacts')
-PATH_ARRAY_DIR = os.path.join(PATH_USER_PROJECT_DIR,'artifacts')
-PATH_AUDIO_DIR = os.path.join(PATH_USER_PROJECT_DIR,'artifacts')
-PATH_FIGURE_DIR = os.path.join(PATH_USER_PROJECT_DIR,'artifacts')
-PATH_TABULAR_DIR = os.path.join(PATH_USER_PROJECT_DIR,'artifacts')
-PATH_VIDEO_DIR = os.path.join(PATH_USER_PROJECT_DIR,'artifacts')
-PATH_IMAGE_DIR = os.path.join(PATH_USER_PROJECT_DIR,'artifacts')
+PATH_ARRAY_DIR = os.path.join(PATH_USER_PROJECT_DIR,'array')
+PATH_AUDIO_DIR = os.path.join(PATH_USER_PROJECT_DIR,'audio')
+PATH_FIGURE_DIR = os.path.join(PATH_USER_PROJECT_DIR,'figure')
+PATH_TABULAR_DIR = os.path.join(PATH_USER_PROJECT_DIR,'tabular')
+PATH_VIDEO_DIR = os.path.join(PATH_USER_PROJECT_DIR,'video')
+PATH_IMAGE_DIR = os.path.join(PATH_USER_PROJECT_DIR,'image')
 
 PATH_DATASET_DIR = os.path.join(PATH_USER_PROJECT_DIR,'dataset')
 PATH_MODEL_DIR = os.path.join(PATH_USER_PROJECT_DIR,'model')
